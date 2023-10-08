7ab59da4333241c91c39ddca3c47f73afcf1b0cb
TOAAdebiyi <129614297+TOAAdebiyi@users.noreply.github.com>
2023-10-08 18:52:34 -0400

    Add files via upload

---
+++ SE_summer_23_Day_14_NodeJS_stream_buffer/App.js
@@ -0,0 +1,75 @@
+
+const fs = require('fs')
+// importing the http module 
+
+const http= require('http')
+
+//  create a readable stream to read file readme.txt. This will fill up the buffer //
+
+const readStream = fs.createReadStream('readme.txt')
+const writeStream = fs.createWriteStream('write.txt')
+
+//  PIPPING
+
+const server = http.createServer((request, response)=>{
+    response.writeHead(200, {"content-Type": "text/html"});
+    const url = request.url
+
+    if(url==='/home'|| url==='/'){
+        fs.createReadStream('index.html').pipe(response)
+    }
+    else if(url==='/about'){
+    fs.createReadStream('about.html').pipe(response)
+}
+else{
+    fs.createReadStream('404.html').pipe(response)
+}
+
+})
+
+//server listening to port 3000
+
+server.listen((3000), ()=>{
+    console.log('server is running at localHost: 3000')
+})
+
+/*
+
+const { response } = require('express')
+
+// creating server in NodeJS
+
+const server = http.createServer((http.request, response)=>{
+    // sending response
+    response.write("This is the response from the server")
+    response.end('\n------End------\n')
+    console.log(http.request.url)
+})
+
+//  STREAM AND BUFFERS //
+//  create a readable stream to read file readme.txt. This will fill up the buffer //
+/*
+
+//  Listen to the stream to read file readme.txt. This will fill up the buffer:
+
+readStream.on('data', (d)=>{
+    console.log('\n--------------------new data received---------------------- \n')
+    console.log(d)
+    console.log('\n------------------------------------------------------------ \n')
+    WriteStream.write(d)
+})
+
+readStream.on('open', ()=>{
+    console.log('\n\nStream opened')
+})
+
+readStream.on('end', ()=>{
+    console.log('Stream closed!.... \n\n')
+})
+
+//  writestream //
+
+const writestream = fs.createWriteStream('write.txt')
+writestream.write('streaming text content...', 'utf-8')
+
+/*
---
+++ SE_summer_23_Day_14_NodeJS_stream_buffer/readme.txt
@@ -0,0 +1,7 @@
+The Lagos State brief is a part of a larger review of social norms, women’s economic empowerment, and policy across six states – Abia, Borno, Edo, Kaduna, Lagos, and Niger – from varied geo-political zones in Nigeria. These states were chosen to reflect diverse contexts among Bill & Melinda Gates Foundation (BMGF) priority states. The full report consisted of a desk review, 36 in-depth interviews (IDIs), and two focus group discussions (FGD) with key stakeholders across the selected states and at the national level.
+Tags:
+Economic empowerment, Law and policy, Gender roles, Health, Social protection, Financial inclusion
+Countries / Regions:
+Nigeria
+Data collection methods:
+Interview, Focus group
